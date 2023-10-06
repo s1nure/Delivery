1890048cdce28aa41897c55c66df7b595b3d0f95
Volodumur Ivanov <s1nure000@gmail.com>
2023-10-07 00:33:13 +0200

    Done Pr 4_1

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ const shopingButton = document.querySelector("#shoping-button");
 const modal = document.querySelector(".modal");
 const modalDialog = document.querySelector(".modal-dialog");
 const closeButton = document.querySelector(".button-close");
-shopingButton.addEventListener("click", function (event) { 
+shopingButton.addEventListener("click", function (event) {
     modal.classList.add("active");
-   
+
 });
 
 closeButton.addEventListener("click", function (event) {
@@ -12,3 +12,79 @@ closeButton.addEventListener("click", function (event) {
 });
 
 new WOW().init();
+
+const buttonAuth = document.querySelector('.button-auth');
+const buttonCloseAuth = document.querySelector(".close-auth");
+const modalAuth = document.querySelector(".modal-auth");
+
+const logInform = document.querySelector("#logInform");
+const inputLogin = document.querySelector("#login");
+const inputPass = document.querySelector("#password");
+const buttonOut = document.querySelector(".button-out");
+const userName = document.querySelector(".user-name")
+
+function toggleAuth() {
+    modalAuth.classList.toggle('active');
+    buttonAuth.style.display = 'none';
+}
+let login = localStorage.getItem('gloDelivery');
+
+function checkOut() {
+    if (login) {
+        authorized();
+    }
+    else {
+        notAuthorized();
+    }
+}
+checkOut();
+
+function notAuthorized() {
+    console.log('Не авторизован')
+
+
+    function logIn(event) {
+        event.preventDefault();
+        login = inputLogin.value;
+        if(!inputLogin.value || !inputPass.value){
+
+            alert("Логін або пароль не були введені!!")
+            return;
+        }
+        localStorage.setItem('gloDelivery',login);
+
+        toggleAuth();
+        logInform.removeEventListener("submit", logIn);
+        buttonAuth.removeEventListener('click', toggleAuth);
+        buttonCloseAuth.removeEventListener('click', toggleAuth);
+        logInform.reset();
+        checkOut();
+
+    }
+    logInform.addEventListener("submit", logIn);
+    buttonAuth.addEventListener('click', toggleAuth);
+    buttonCloseAuth.addEventListener('click', toggleAuth);
+
+}
+function authorized() {
+    console.log('Авторизован');
+
+    function logOut() {
+        login = null;
+        buttonAuth.style.display = "";
+        buttonOut.style.display = "";
+        userName.style.display = "";
+        localStorage.removeItem('gloDelivery');
+        buttonOut.removeEventListener('click', logOut);
+        checkOut();
+
+    }
+    userName.textContent = login;
+    buttonAuth.style.display = "none";
+    buttonOut.style.display = "block";
+    userName.style.display = "block";
+    buttonOut.addEventListener('click', logOut);
+
+}
+
+
