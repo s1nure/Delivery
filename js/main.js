ae7df10ae7140fd4e874d8a87bbde132d8f30e6a
Volodumur Ivanov <s1nure000@gmail.com>
2023-11-03 23:02:30 +0100

    Update login form and modal window

--- js/main.js
+++ js/main.js
@@ -1,89 +1,101 @@
-const shopingButton = document.querySelector("#shoping-button");
-const modal = document.querySelector(".modal");
-const modalDialog = document.querySelector(".modal-dialog");
-const closeButton = document.querySelector(".button-close");
-shopingButton.addEventListener("click", function (event) {
-    modal.classList.add("active");
+const shopingButton = document.querySelector('#shoping-button')
+const modal = document.querySelector('.modal')
+const modalDialog = document.querySelector('.modal-dialog')
+const closeButton = document.querySelector('.button-close')
+shopingButton.addEventListener('click', function (event) {
+	modal.classList.add('active')
+})
 
-});
+closeButton.addEventListener('click', function (event) {
+	modal.classList.remove('active')
+})
 
-closeButton.addEventListener("click", function (event) {
-    modal.classList.remove("active")
-});
+new WOW().init()
 
-new WOW().init();
+const buttonAuth = document.querySelector('.button-auth')
+const buttonCloseAuth = document.querySelector('.close-auth')
+const modalAuth = document.querySelector('.modal-auth')
 
-const buttonAuth = document.querySelector('.button-auth');
-const buttonCloseAuth = document.querySelector(".close-auth");
-const modalAuth = document.querySelector(".modal-auth");
-
-const logInform = document.querySelector("#logInform");
-const inputLogin = document.querySelector("#login");
-const inputPass = document.querySelector("#password");
-const buttonOut = document.querySelector(".button-out");
-const userName = document.querySelector(".user-name")
+const logInform = document.querySelector('#logInform')
+const inputLogin = document.querySelector('#login')
+const inputPass = document.querySelector('#password')
+const buttonOut = document.querySelector('.button-out')
+const userName = document.querySelector('.user-name')
 
 function toggleAuth() {
-    modalAuth.classList.toggle('active');
+	modalAuth.classList.toggle('active')
+	inputLogin.style.borderColor = ''
+	inputPass.style.borderColor = ''
+
+    if (modalAuth.classList.contains('active')) {
+		window.disableScroll()
+	} else {
+		window.enableScroll()
+	}
 }
-let login = localStorage.getItem('gloDelivery');
+let login = localStorage.getItem('gloDelivery')
 
 function checkOut() {
-    if (login) {
-        authorized();
-    }
-    else {
-        notAuthorized();
-    }
+	if (login) {
+		authorized()
+	} else {
+		notAuthorized()
+	}
 }
-checkOut();
+checkOut()
 
 function notAuthorized() {
-    console.log('Не авторизован')
+	console.log('Не авторизован')
 
-
-    function logIn(event) {
-        event.preventDefault();
-        login = inputLogin.value;
-        if(!inputLogin.value || !inputPass.value){
-
-            alert("Логін або пароль не були введені!!")
-            return;
-        }
-        localStorage.setItem('gloDelivery',login);
-        buttonAuth.style.display = 'none';
-        toggleAuth();
-        logInform.removeEventListener("submit", logIn);
-        buttonAuth.removeEventListener('click', toggleAuth);
-        buttonCloseAuth.removeEventListener('click', toggleAuth);
-        logInform.reset();
-        checkOut();
-
-    }
-    logInform.addEventListener("submit", logIn);
-    buttonAuth.addEventListener('click', toggleAuth);
-    buttonCloseAuth.addEventListener('click', toggleAuth);
-
+	function logIn(event) {
+		event.preventDefault()
+		login = inputLogin.value
+		if (!inputLogin.value.trim() || !inputPass.value.trim()) {
+			alert('Логін або пароль не були введені!!')
+			if (!inputLogin.value.trim()) {
+				inputLogin.style.borderColor = '#ff0000'
+				inputLogin.value = ''
+            }
+            
+			if (!inputPass.value.trim()) {
+				inputPass.style.borderColor = '#ff0000'
+				inputPass.value = ''
+			}
+			return
+		}
+		localStorage.setItem('gloDelivery', login)
+		buttonAuth.style.display = 'none'
+		toggleAuth()
+		logInform.removeEventListener('submit', logIn)
+		buttonAuth.removeEventListener('click', toggleAuth)
+		buttonCloseAuth.removeEventListener('click', toggleAuth)
+		logInform.reset()
+		checkOut()
+	}
+	logInform.addEventListener('submit', logIn)
+	buttonAuth.addEventListener('click', toggleAuth)
+	buttonCloseAuth.addEventListener('click', toggleAuth)
+	modalAuth.addEventListener('click', function (event) {
+		if (event.target.classList.contains('active')) {
+			toggleAuth()
+		}
+	})
 }
 function authorized() {
-    console.log('Авторизован');
+	console.log('Авторизован')
 
-    function logOut() {
-        login = null;
-        buttonAuth.style.display = "";
-        buttonOut.style.display = "";
-        userName.style.display = "";
-        localStorage.removeItem('gloDelivery');
-        buttonOut.removeEventListener('click', logOut);
-        checkOut();
-
-    }
-    userName.textContent = login;
-    buttonAuth.style.display = "none";
-    buttonOut.style.display = "block";
-    userName.style.display = "block";
-    buttonOut.addEventListener('click', logOut);
-
+	function logOut() {
+		login = null
+		buttonAuth.style.display = ''
+		buttonOut.style.display = ''
+		userName.style.display = ''
+		localStorage.removeItem('gloDelivery')
+		buttonOut.removeEventListener('click', logOut)
+		checkOut()
+	}
+	userName.textContent = login
+	buttonAuth.style.display = 'none'
+	buttonOut.style.display = 'block'
+	userName.style.display = 'block'
+	buttonOut.addEventListener('click', logOut)
 }
-
-
